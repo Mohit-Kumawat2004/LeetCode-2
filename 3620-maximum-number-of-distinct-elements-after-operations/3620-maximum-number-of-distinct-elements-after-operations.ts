17b9bbff9da26712f219d50863641c1dc6b05d80
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2025-10-18 09:51:21 +0530

    Time: 133 ms (60%), Space: 68.4 MB (80%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.ts
@@ -0,0 +1,17 @@
+function maxDistinctElements(nums: number[], k: number): number {
+    if (nums.length === 0) return 0;
+    nums.sort((a, b) => a - b);
+    let count = 0;
+    let prev = -2147483648 >> 1;
+    for (let a of nums) {
+        let low = a - k;
+        let high = a + k;
+        let x = prev + 1;
+        if (x < low) x = low;
+        if (x <= high) {
+            count++;
+            prev = x;
+        }
+    }
+    return count;
+}
