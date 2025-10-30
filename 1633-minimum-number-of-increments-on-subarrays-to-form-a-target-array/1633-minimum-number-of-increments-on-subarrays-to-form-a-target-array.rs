08372a568e16b0dd5a7a6fc8ac7877e8c04a9bf8
Mohit Kumawat <159867125+Mohit-Kumawat2004@users.noreply.github.com>
2025-10-30 20:27:45 +0530

    Time: 0 ms (100%), Space: 3.3 MB (70%) - LeetHub

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn min_number_operations(target: Vec<i32>) -> i32 {
+        let n=target.len();
+        let mut ans=target[0];
+        for i in 1..n{
+            ans+=(target[i]-target[i-1]).max(0);
+        }
+        ans
+    }
+}
